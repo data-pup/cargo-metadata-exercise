756fc3afe4e803d5bfd37de7d60ba1d616657ede
data-pup <kevinmartinucr@gmail.com>
2018-09-04 17:17:30 -0400

    Lockfile can be printed out now!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 extern crate cargo_metadata;
-// #[macro_use]
-// extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 // extern crate serde_json;
-// extern crate toml;
+extern crate toml;
 
 use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
 // FIXUP: Define lockfile structure.
+#[derive(Debug, Deserialize)]
+struct _CargoLockfile {
+    package: Vec<_CargoLockfileDependency>,
+}
+
+/// FIXUP: This almost certainly needs some revision.
+#[derive(Debug, Deserialize)]
+struct _CargoLockfileDependency {
+    name: String,
+    version: String,
+    source: Option<String>,
+    dependencies: Option<Vec<String>>
+}
+
 
 /// Given the path to the crate that we are buliding, return a path to the
 /// lock file, by finding the workspace root.
@@ -22,22 +36,21 @@ fn _get_lockfile_path(path: Option<PathBuf>) -> PathBuf {
 }
 
 /// Read the `Cargo.lock` file found at the given location.
-fn _read_cargo_lock(path: &Path) -> Result<u8, std::io::Error> {
-    let manifest_path = path.join("Cargo.toml");
-    if !manifest_path.is_file() {
+fn _read_cargo_lock(path: &Path) -> _CargoLockfile {
+    if !path.is_file() {
         // FIXUP: Add error handling here.
         panic!("BORKED: Couldn't find the `Cargo.lock` file.");
     }
-    let mut cargo_file = File::open(manifest_path)?;
+    let mut cargo_file = File::open(path).unwrap();
     let mut cargo_contents = String::new();
-    cargo_file.read_to_string(&mut cargo_contents)?;
+    cargo_file.read_to_string(&mut cargo_contents).unwrap();
 
-    unimplemented!();
-    // Ok(toml::from_str(&cargo_contents)?)
+    toml::from_str(&cargo_contents).unwrap()
 }
 
 fn main() {
     let lockfile_path = _get_lockfile_path(None);
-    let output = format!("PATH: `{:?}`", lockfile_path);
-    println!("{}", output);
+    println!("{}", format!("PATH: `{:?}`", lockfile_path));
+    let _lock_file = _read_cargo_lock(&lockfile_path);
+    println!("{:?}", _lock_file);
 }
